fc657307cba28957ff87b293e84313e40cfa12f2
subbum <subbu@gmail.com>
2023-06-19 14:45:55 +0100

    elastic cache service added

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+#Creating Elastic Cache Cluster
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id           = "${var.env}-elasticache"
+  engine               = var.engine
+  engine_version       = var.engine_version
+  node_type            = var.node_type
+  num_cache_nodes      = var.nun_cache_nodes
+  port                 = 6379
+  subnet_group_name = aws_elasticache_subnet_group.main.name
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-elasticache" }
+  )
+}
+
+
+##Elasticache SubnetGroup for Elastic Cache
+resource "aws_elasticache_subnet_group" "main" {
+  name       =  "${var.env}-elasticache"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-subnet-group" }
+  )
+}
