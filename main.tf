f0f6483800a629313b6770a859b9718be98815ac
subbum <subbu@gmail.com>
2023-06-28 21:24:24 +0100

    elasticache updated with SG and SSM parameters

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_elasticache_cluster" "elasticache" {
   num_cache_nodes      = var.nun_cache_nodes
   port                 = 6379
   subnet_group_name = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
@@ -28,4 +29,41 @@ resource "aws_elasticache_subnet_group" "main" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+
+
+##Add aws_ssm parameter elasticache endpoint
+resource "aws_ssm_parameter" "elasticache_endpoint" {
+  name  = "${var.env}.elasticache.endpoint"
+  type  = "String"
+  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+}
+
+#Creating SG for accept connnections
+resource "aws_security_group" "main" {
+  name        = "elasticache-${var.env}"
+  description = "elasticache-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "ELASTICACHE"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "elasticache-${var.env}" }
+  )
 }
--- vars.tf
+++ vars.tf
@@ -5,3 +5,6 @@
   variable "env" {}
   variable "tags" {}
   variable "subnet_ids" {}
+
+  variable "vpc_id" {}
+  variable "allow_subnets"{}
